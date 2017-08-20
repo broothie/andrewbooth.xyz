b22cfb7c6b3324899092321e6ea55b660bbe95c2
Andrew Booth <boothandrewd@gmail.com>
2017-08-20 13:59:08 -0700

    Site skeleton

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  context: __dirname,
+  entry: './src/js/main.js',
+  output: {
+    filename: './assets/js/bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '*']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-maps'
+};
