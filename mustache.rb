b1956b03d996da02e868e84e40df0acc2acb7aff
Andrew Booth <booth@shipt.com>
2019-12-03 00:37:38 -0800

    parcel

--- mustache.rb
+++
@@ -1,9 +0,0 @@
-require 'mustache'
-require 'yaml'
-
-template = File.read('index.mustache.html')
-data = YAML.load_file('site.yml')
-
-html = Mustache.render(template, data)
-File.write('index.html', html)
-puts "Wrote #{html.length} to index.html"
---
+++ src/pug.config.js
@@ -0,0 +1,60 @@
+exports.locals = {
+    projects: [
+        {
+            title: 'Andrew Booth',
+            description: `
+                Software Engineer at <a href="https://shipt.com">Shipt</a>.
+                <br/>
+                <br/>
+                <br/>
+                <a href="assets/docs/AndrewBooth_resume.pdf">Resume</a>
+                <br/>
+                <a href="https://github.com/broothie">Github</a>
+                <br/>
+                <a href="https://linkedin.com/in/broothie">LinkedIn</a>
+            `,
+            live_link: 'https://github.com/broothie',
+            image_url: 'assets/img/sugarbush-mugshot.jpeg'
+        },
+        {
+            title: 'werds.co',
+            description: 'A place for words.',
+            live_link: 'https://werds.co',
+            image_url: 'assets/img/werds.co.gif'
+        },
+        {
+            title: 'Triangulunch',
+            description: 'Meet in the middle for lunch!',
+            live_link: 'https://triangulun.ch',
+            image_url: 'assets/img/triangulun.ch.gif'
+        },
+        {
+            title: 'fileserver',
+            description: 'A file server, written in Go. Helpful for developing static websites.',
+            live_link: 'https://github.com/broothie/fileserver',
+            image_url: 'assets/img/fileserver.gif'
+        },
+        {
+            title: 'slink.chat',
+            description: `
+                An AIM clone visually influenced by Windows 95.
+                Built on
+                <a href="http://rubyonrails.org/" target="_blank">Rails</a>,
+                <a href="https://www.postgresql.org/" target="_blank">PostgreSQL</a>,
+                <a href="https://facebook.github.io/react/" target="_blank">React</a>,
+                and <a href="http://redux.js.org/" target="_blank">Redux</a>.
+            `,
+            live_link: 'http://www.slink.chat',
+            image_url: 'assets/img/slink.gif'
+        },
+        {
+            title: 'Primordial Playground',
+            description: `
+                A colorful, interactive Game of Life sandbox.
+                Built with <a href="http://vanilla-js.com/">vanilla js</a>.
+            `,
+            live_link: 'https://broothie.github.io/primordial-playground/',
+            image_url: 'assets/img/primordial-playground.gif'
+        }
+    ]
+};
